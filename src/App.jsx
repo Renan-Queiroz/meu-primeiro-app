3d2d8016f4f7147c1beb4631c0b869a816665b00
renan queiroz <renanqueiroz1464@gmail.com>
2025-09-26 21:35:05 -0300

    estatos e eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import { Card } from "./components/Card";
 import { Hello } from "./components/Hello";
 
-const CARDS =[
+
+const CARDS = [
   { id: 0, title: 'Card A', description: 'Filho de A' },
   { id: 1, title: 'Card B', description: 'Filho de B' },
   { id: 2, title: 'Card C', description: 'Filho de C' },
 ]
 
-
-
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const inc = () => setCount(c => c + 1)
+
   return (
     <main className="min-h-dvh bg-slate-50">
       <h1 className="text-3xl font-bold text-slate-800">
@@ -29,6 +33,10 @@ const App = () => {
       <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-100 focus:ring-2 focust:ring-blue-500">
         Link com foco
       </a>
+
+      <button onClick={inc} className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-100 focus:ring-2 focust:ring-blue-500">
+        Contador: {count}
+      </button>
 
       <Hello name="Renan Queiróz" />
 
